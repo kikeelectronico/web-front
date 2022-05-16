cd1470ccf751e8e67d9e2365f8a66619a3a1678c
enrique.gomez <enrique.gomez@bq.com>
2022-05-16 20:53:55 +0200

    multiple labels for projects and WIP label

--- web-front/src/components/Label.js
+++ web-front/src/components/Label.js
@@ -4,7 +4,11 @@ import "./label.css"
 export default function Label(props) {
 
   return (
-    <div className="label">
+    <div
+      className="label"
+      style={{backgroundColor: props.label == "WIP" ? "red" : "#0573e1"}}
+      title={props.label == "WIP" ? "Work in progress" : props.label}
+    >
       {props.label}
     </div>
   )
