7804b307dc5c57876ea6631e2d14fc4b886e6994
enrique.gomez <enrique.gomez@bq.com>
2022-05-16 19:44:16 +0200

    projects

--- web-front/src/App.js
+++ web-front/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar';
 import Hello from './components/Hello';
 import SectionTitle from './components/SectionTitle';
+import Projects from './components/Projects';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Hello/>
       <SectionTitle title="Proyectos de hardware" image="soldering.png"/>
+      <Projects/>
     </div>
   );
 }
---
+++ web-front/src/components/Projects.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import "./projects.css"
+
+const API = "http://" + window.location.hostname + ":8000"
+
+export default function Projects() {
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = () => {
+    fetch(API + "/projects/?type=hardware")
+    .then((response) => response.json())
+    .then((data) => setProjects(data))
+    .catch((error) => console.log(error))
+  }
+
+  return (
+    <div className="projects-container">
+      {
+        projects.map((project) => {
+          return (
+            <div key={project.title} className="project-card">
+              <h3 className="project-title">
+                {project.title}
+              </h3>
+              <p className="project-description">
+                {project.description}
+              </p>
+              <div className="project-buttons">
+              {
+                project.buttons.map((button) => {
+                  return (
+                    <a className="project-button" href={button.url} target="_blanck">
+                        {button.title}
+                    </a>
+                  )
+                })
+              }
+              </div>
+
+              <img className="project-image" src={project.image}/>
+
+            </div>
+          )
+        })
+      }
+    </div> 
+  )
+}
