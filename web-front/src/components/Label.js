a65ba16f985c59d423dc8cc64fd71a20c7fc68cb
enrique.gomez <enrique.gomez@bq.com>
2022-05-17 23:16:45 +0200

    ===

--- web-front/src/components/Label.js
+++ web-front/src/components/Label.js
@@ -6,8 +6,8 @@ export default function Label(props) {
   return (
     <div
       className="label"
-      style={{backgroundColor: props.label == "WIP" ? "red" : "#0573e1"}}
-      title={props.label == "WIP" ? "Work in progress" : props.label}
+      style={{backgroundColor: props.label === "WIP" ? "red" : "#0573e1"}}
+      title={props.label === "WIP" ? "Work in progress" : props.label}
     >
       {props.label}
     </div>
