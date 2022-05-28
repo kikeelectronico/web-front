4ca3cf09655a0ba916bf7a37dc2bfd7c0ed7f061
enrique.gomez <enrique.gomez@bq.com>
2022-05-29 00:18:11 +0200

    introduction

--- web-front/src/components/Hello.js
+++ web-front/src/components/Hello.js
@@ -8,9 +8,16 @@ export default function Hello() {
       <div className="hello-column">
         <div className="hello-content">
           <h2>Electro hola</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec dolor sagittis, dignissim magna vel, tincidunt ligula. Fusce enim augue, scelerisque nec magna vel, porta mattis odio. Vivamus imperdiet ex et nibh convallis, vel tincidunt nunc varius. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse potenti.
-          </p><p>
-          Nulla finibus, leo blandit venenatis iaculis, lorem odio commodo neque, non euismod mauris mauris vitae est. Fusce scelerisque quis eros id fermentum. Quisque fermentum nulla sed erat consectetur, sed auctor enim consequat.</p>
+          <p>
+            Soy Enrique, un ingeniero electrónico enamorado del diseño y fabricación de hardware.
+          </p>
+          <p>
+            Trabajo en el sector de la educación creando contenidos sobre tecnología, eligíendo la más adecuada para el proyecto y creándola cuando no existe. Dedico la mayoría de mi jornada a un proyecto realmente grande para el que he creado contenido, hardware, firmware, web apps y apps móviles.
+          </p>
+          <p>
+            Me gustan los retos ingenieriles, pero me encanta cuando están relacionados con el diseño de hardware.
+          </p>
+          <p>Todo lo hago <i>Open Source</i> si me dejan.</p>
         </div>
       </div>
       <div className="hello-column">
