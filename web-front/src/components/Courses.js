9517178acfa692b77362017aa11435e556b25720
enrique.gomez <enrique.gomez@bq.com>
2023-02-08 22:00:55 +0100

    add loading text

--- web-front/src/components/Courses.js
+++ web-front/src/components/Courses.js
@@ -20,43 +20,54 @@ export default function Courses(props) {
   }
 
   return (
-    <section className="courses-container" id={props.id}>
+    <div>
       {
-        courses.slice(0,max_cards).map((course) => {
-          return (
-            <div key={course.title} className="course-card">
-              <h3 className="course-title">
-                {course.title}
-              </h3>
-              <p className="course-description">
-                {course.description}
-              </p>
-              <div className="course-buttons">
+        courses.length === 0
+        ?
+          <section className="courses-loading-container" id={props.id}>
+            <h2>Cargando...</h2>
+          </section>
+        :
+          <section className="courses-section" id={props.id}>
+            <div className="courses-container">
               {
-                course.buttons ?
-                  course.buttons.map((button) => {
-                    return (
-                      <a className="course-button" href={button.url} target="_blanck" key={button.title}>
-                          {button.title}
-                      </a>
-                    )
-                  })
+                courses.slice(0,max_cards).map((course) => {
+                  return (
+                    <div key={course.title} className="course-card">
+                      <h3 className="course-title">
+                        {course.title}
+                      </h3>
+                      <p className="course-description">
+                        {course.description}
+                      </p>
+                      <div className="course-buttons">
+                      {
+                        course.buttons ?
+                          course.buttons.map((button) => {
+                            return (
+                              <a className="course-button" href={button.url} target="_blanck" key={button.title}>
+                                  {button.title}
+                              </a>
+                            )
+                          })
+                        : <></>
+                      }
+                      </div>
+                    </div>
+                  )
+                })
+              }
+              {
+                max_cards !== courses.length && courses.length > max_cards ?
+                <div key="plus" className="course-card-plus" onClick={() => {setMaxCards(courses.length)}}>
+                  <span className="course-plus">+</span>
+                  <span className="course-plus-text">Ver más cursos</span>
+                </div>
                 : <></>
               }
-              </div>
-            </div>
-          )
-        })
+            </div>            
+          </section> 
       }
-      {
-        max_cards !== courses.length && courses.length > 3 ?
-        <div key="plus" className="course-card-plus" onClick={() => {setMaxCards(courses.length)}}>
-          <span className="course-plus">+</span>
-          <span className="course-plus-text">Ver más cursos</span>
-        </div>
-        : <></>
-      }
-      
-    </section> 
+    </div>
   )
 }
--- web-front/src/components/Projects.js
+++ web-front/src/components/Projects.js
@@ -23,53 +23,61 @@ export default function Projects(props) {
 
   return (
     <div>
-      <section className="projects-container" id={props.id}>
         {
-          projects.slice(0,max_cards).map((project) => {
-            return (
-              <div key={project.title} className="project-card">
-                <div className="project-labels">
-                  {
-                    project.type.map(label => {
-                      return (<Label key={label} label={label}/>)
-                    })
-                  }
-                </div>
-                <h3 className="project-title">
-                  {project.title}
-                </h3>
-                <p className="project-description">
-                  {project.description}
-                </p>
-                <div className="project-buttons">
+          projects.length === 0
+          ?
+            <section className="project-loading-container" id={props.id}>
+              <h2>Cargando...</h2>
+            </section>
+          :
+            <section className="projects-section" id={props.id}>
+              <div className="projects-container">
                 {
-                  project.buttons.map((button) => {
+                  projects.slice(0,max_cards).map((project) => {
                     return (
-                      <a className="project-button" href={button.url} target="_blanck" key={button.title}>
-                          {button.title}
-                      </a>
+                      <div key={project.title} className="project-card">
+                        <div className="project-labels">
+                          {
+                            project.type.map(label => {
+                              return (<Label key={label} label={label}/>)
+                            })
+                          }
+                        </div>
+                        <h3 className="project-title">
+                          {project.title}
+                        </h3>
+                        <p className="project-description">
+                          {project.description}
+                        </p>
+                        <div className="project-buttons">
+                        {
+                          project.buttons.map((button) => {
+                            return (
+                              <a className="project-button" href={button.url} target="_blanck" key={button.title}>
+                                  {button.title}
+                              </a>
+                            )
+                          })
+                        }
+                        </div>
+
+                        <img className="project-image" alt="Proyecto" src={project.image}/>
+
+                      </div>
                     )
                   })
                 }
-                </div>
-
-                <img className="project-image" alt="Proyecto" src={project.image}/>
-
               </div>
-            )
-          })
+              {
+                max_cards !== projects.length && projects.length > max_cards ?
+                <div key="plus" className="project-card-plus" onClick={() => {setMaxCards(projects.length)}}>
+                  <span className="project-plus">+</span>
+                  <span className="project-plus-text">Ver más proyectos</span>
+                </div>
+                : <></>
+              }
+            </section>
         }
-        
-        
-      </section> 
-      {
-        max_cards !== projects.length && projects.length > 3 ?
-        <div key="plus" className="project-card-plus" onClick={() => {setMaxCards(projects.length)}}>
-          <span className="project-plus">+</span>
-          <span className="project-plus-text">Ver más proyectos</span>
-        </div>
-        : <></>
-      }
     </div>
   )
 }
