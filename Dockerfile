13e2da7c5294b0ab9abddcbe62423cc7560597ba
Enrique <enrique.gomez.electronico@gmail.com>
2023-11-17 21:49:53 +0100

    set api using an env

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM node:lts-alpine AS base
+ARG REACT_APP_API_URL
+ENV REACT_APP_API_URL=$REACT_APP_API_URL
 RUN mkdir front
 WORKDIR /front
 COPY web-front ./
--- web-front/src/constants.js
+++ web-front/src/constants.js
@@ -1,3 +1,4 @@
-const API = window.location.hostname === "localhost" ? "http://" + window.location.hostname + ":8000" : "https://api.enriquegomez.me"
+// const API = window.location.hostname === "localhost" ? "http://" + window.location.hostname + ":8000" : "https://api.enriquegomez.me"
+// // const API = process.env.REACT_APP_API_URL;
 
-export default API 
+// export default API 
