9e09793c824d35978c9b82b16ba25ed5b8454ea7
Enrique <enrique.gomez.electronico@gmail.com>
2023-11-18 20:39:59 +0100

    change navbar

--- web-front/src/components/Navbar.js
+++ web-front/src/components/Navbar.js
@@ -3,34 +3,14 @@ import "./navbar.css"
 
 export default function Navbar() {
 
-  const [active, setActive] = useState("hello");
+  const [active, setActive] = useState("");
   const [menu, setMenu] = useState(false);
 
   useEffect(() => {
-    highLight();
+    let slug = window.location.href.split("/")[3]
+    setActive(slug.length == 0 ? "hello" : slug)
+
   }, [])
-
-  const highLight = () => {
-
-    const sections = document.querySelectorAll("section");
-
-    window.addEventListener("scroll", () => {
-      const scrollY = window.pageYOffset;
-      if (scrollY < 200) {
-        setActive("hello")
-      } else {
-        sections.forEach(section => {
-          const section_height = section.offsetHeight;
-          const section_top = section.offsetTop - 100;
-          if(scrollY > section_top && scrollY < section_top + section_height) {
-            const section_id = section.getAttribute('id')
-            setActive(section_id)
-          }
-
-        })
-      }
-    })
-  }
 
   const toggleMenu = () => {
     setMenu(!menu)
@@ -43,28 +23,13 @@ export default function Navbar() {
             <a onClick={() => {toggleMenu()}} >{menu ? "X" : "Menú"}</a>
         </div>
         <div className={"navbar-option " + (active === "hello" ? "navbar-option-active" : "")}>
-          <a href="#hello">Hola</a>
+          <a href="/">Hola</a>
         </div>
-        <div className={"navbar-option " + (active === "hardware" ? "navbar-option-active" : "")}>
-          <a href="#hardware">Proyectos de hardware</a>
+        <div className={"navbar-option " + (active === "portfolio" ? "navbar-option-active" : "")}>
+          <a href="/portfolio">Portfolio</a>
         </div>
-        <div className={"navbar-option " + (active === "software" ? "navbar-option-active" : "")}>
-          <a href="#software">Proyectos de software</a>
-        </div>
-        <div className={"navbar-option " + (active === "others" ? "navbar-option-active" : "")}>
-          <a href="#others">Otros proyectos</a>
-        </div>
-        {/*<div className={"navbar-option " + (active === "experience" ? "navbar-option-active" : "")}>
-          <a href="#experience">Experiencia</a>
-        </div>
-        <div className={"navbar-option " + (active === "skills" ? "navbar-option-active" : "")}>
-          <a href="#skills">Habilidades</a>
-        </div>*/}
-        <div className={"navbar-option " + (active === "courses" ? "navbar-option-active" : "")}>
-          <a href="#courses">Formación</a>
-        </div>
-        <div className={"navbar-option " + (active === "interviews" ? "navbar-option-active" : "")}>
-          <a href="#interviews">Entrevistas</a>
+        <div className={"navbar-option " + (active === "blog" ? "navbar-option-active" : "")}>
+          <a href="/blog">Blog</a>
         </div>
       </div>
     </nav>
