de4bf0f39d0fc504e9b612d4afe1d36fdba12d52
Enrique <enrique.gomez.electronico@gmail.com>
2024-03-30 19:01:03 +0100

    add PGP public key

--- web-front/src/pages/Contact.js
+++ web-front/src/pages/Contact.js
@@ -12,6 +12,12 @@ export default function Contact() {
           <h3>Puedes escribirme usando LinkedIn</h3>
           <a className="contact-button" href="https://www.linkedin.com/in/enrique-gomez-rodriguez-elec/" target="_blank">Escríbeme</a>
         </div>
+      </section>
+      <SectionTitle title="Clave PGP pública" image="candado.png"/>
+      <section className="contact-container">
+        <div className="contact-card">
+          <a className="contact-button" href="https://assets.enriquegomez.me/enrique_gomez.asc" target="_blank">Descargar</a>
+        </div>
       </section> 
     </div>
   )
