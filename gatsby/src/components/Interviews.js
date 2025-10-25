bf88a1b07457dba007d83324d7c5823e4473acde
kikeelectronico <enrique.gomez.electronico@gmail.com>
2025-10-25 21:53:27 +0200

    remove loading title

--- gatsby/src/components/Interviews.js
+++ gatsby/src/components/Interviews.js
@@ -54,65 +54,54 @@ export default function Interviews(props) {
   })
 
   return (
-    <>
+    <section className="interviews-section" id={props.id}>
+      <div className="interviews-container">
+        {
+          interviews.slice(0,max_cards).map((interview) => {
+
+            const baseName = interview.parent.name 
+            const image = imagesMap[baseName]
+
+            return (
+              <div key={interview.title} className="interview-card">
+                <h3 className="interview-title">
+                  {interview.title}
+                </h3>
+                <p className="interview-description">
+                  {interview.description}
+                </p>
+                <div className="interview-buttons">
+                {
+                  interview.buttons.map((button, index) => {
+                    return (
+                      <a className="interview-button" style={{marginRight: index === interview.buttons.length-1 ? 0 : 30}} href={button.url} target="_blanck" key={button.title}>
+                          {button.title}
+                      </a>
+                    )
+                  })
+                }
+                </div>
+
+                <GatsbyImage
+                  image={image}
+                  alt={`Imagen representativa de ${interview.title}`}
+                  className="interview-image"
+                />
+                
+              </div>
+            )
+          })
+        }
+      </div>              
       {
-        interviews.length === 0
-        ?
-          <section className="interviews-loading-container" id={props.id}>
-            <h2>Sin resultados</h2>
-          </section>
-        :
-          <section className="interviews-section" id={props.id}>
-            <div className="interviews-container">
-              {
-                interviews.slice(0,max_cards).map((interview) => {
-
-                  const baseName = interview.parent.name 
-                  const image = imagesMap[baseName]
-
-                  return (
-                    <div key={interview.title} className="interview-card">
-                      <h3 className="interview-title">
-                        {interview.title}
-                      </h3>
-                      <p className="interview-description">
-                        {interview.description}
-                      </p>
-                      <div className="interview-buttons">
-                      {
-                        interview.buttons.map((button, index) => {
-                          return (
-                            <a className="interview-button" style={{marginRight: index === interview.buttons.length-1 ? 0 : 30}} href={button.url} target="_blanck" key={button.title}>
-                                {button.title}
-                            </a>
-                          )
-                        })
-                      }
-                      </div>
-
-                      <GatsbyImage
-                        image={image}
-                        alt={`Imagen representativa de ${interview.title}`}
-                        className="interview-image"
-                      />
-                     
-                    </div>
-                  )
-                })
-              }
-            </div>              
-            {
-              max_cards !== interviews.length && interviews.length > max_cards ?
-              <button key="plus" className="interview-card-plus" onClick={() => {setMaxCards(interviews.length)}}>
-                <span className="interview-plus">+</span>
-                <span className="interview-plus-text">Ver más entrevistas</span>
-              </button>
-              : <></>
-            }
-            
-          </section>
+        max_cards !== interviews.length && interviews.length > max_cards ?
+        <button key="plus" className="interview-card-plus" onClick={() => {setMaxCards(interviews.length)}}>
+          <span className="interview-plus">+</span>
+          <span className="interview-plus-text">Ver más entrevistas</span>
+        </button>
+        : <></>
       }
-    </>
-
+      
+    </section>
   )
 }
