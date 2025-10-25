9a074c7ddd2eed5dbf0e343f03276683ae929c33
kikeelectronico <enrique.gomez.electronico@gmail.com>
2025-10-25 15:23:05 +0200

    remove loading code

--- gatsby/src/components/Courses.js
+++ gatsby/src/components/Courses.js
@@ -28,64 +28,54 @@ export default function Courses(props) {
   const courses = data.allCoursesJson.nodes; 
 
   return (
-    <div>
-      {
-        courses === null
-        ?
-          <section className="courses-loading-container" id={props.id}>
-            <h2>Cargando...</h2>
-          </section>
-        :
-          <>
-          {
-            courses.length === 0
-            ?
-              <section className="courses-loading-container" id={props.id}>
-                <h2>Sin resultados</h2>
-              </section>
-            :
-              <section className="courses-section" id={props.id}>
-                <div className="courses-container">
-                  {
-                    courses.slice(0,max_cards).map((course) => {
-                      return (
-                        <div key={course.title} className="course-card">
-                          <h3 className="course-title">
-                            {course.title}
-                          </h3>
-                          <p className="course-description">
-                            {course.description}
-                          </p>
-                          <div className="course-buttons">
-                          {
-                            course.buttons ?
-                              course.buttons.map((button, index) => {
-                                return (
-                                  <a className="course-button" style={{marginRight: index === course.buttons.length-1 ? 0 : 30}} href={button.url} target="_blanck" key={button.title}>
-                                      {button.title}
-                                  </a>
-                                )
-                              })
-                            : <></>
-                          }
-                          </div>
-                        </div>
-                      )
-                    })
-                  }
-                  {
-                    max_cards !== courses.length && courses.length > max_cards ?
-                    <div key="plus" className="course-card-plus" onClick={() => {setMaxCards(courses.length)}}>
-                      <span className="course-plus">+</span>
-                      <span className="course-plus-text">Ver más cursos</span>
+    <>
+    {
+      courses.length === 0
+      ?
+        <section className="courses-loading-container" id={props.id}>
+          <h2>Sin resultados</h2>
+        </section>
+      :
+        <section className="courses-section" id={props.id}>
+          <div className="courses-container">
+            {
+              courses.slice(0,max_cards).map((course) => {
+                return (
+                  <div key={course.title} className="course-card">
+                    <h3 className="course-title">
+                      {course.title}
+                    </h3>
+                    <p className="course-description">
+                      {course.description}
+                    </p>
+                    <div className="course-buttons">
+                    {
+                      course.buttons ?
+                        course.buttons.map((button, index) => {
+                          return (
+                            <a className="course-button" style={{marginRight: index === course.buttons.length-1 ? 0 : 30}} href={button.url} target="_blanck" key={button.title}>
+                                {button.title}
+                            </a>
+                          )
+                        })
+                      : <></>
+                    }
                     </div>
-                    : <></>
-                  }
-                </div>            
-              </section> 
-          }
-          </>
-      }
-    </div>
+                  </div>
+                )
+              })
+            }
+            {
+              max_cards !== courses.length && courses.length > max_cards ?
+              <div key="plus" className="course-card-plus" onClick={() => {setMaxCards(courses.length)}}>
+                <span className="course-plus">+</span>
+                <span className="course-plus-text">Ver más cursos</span>
+              </div>
+              : <></>
+            }
+          </div>            
+        </section> 
+    }
+    </>
   )
 }
--- gatsby/src/components/Interviews.js
+++ gatsby/src/components/Interviews.js
@@ -27,66 +27,57 @@ export default function Interviews(props) {
 const interviews = data.allInterviewsJson.nodes; 
 
   return (
-    <div>
-       {
-          interviews === null
-          ?
-            <section className="interviews-loading-container" id={props.id}>
-              <h2>Cargando...</h2>
-            </section>
-          :
-            <>
-            {
-              interviews.length === 0
-              ?
-                <section className="interviews-loading-container" id={props.id}>
-                  <h2>Sin resultados</h2>
-                </section>
-              :
-                <section className="interviews-section" id={props.id}>
-                  <div className="interviews-container">
-                    {
-                      interviews.slice(0,max_cards).map((interview) => {
-                        return (
-                          <div key={interview.title} className="interview-card">
-                            <h3 className="interview-title">
-                              {interview.title}
-                            </h3>
-                            <p className="interview-description">
-                              {interview.description}
-                            </p>
-                            <div className="interview-buttons">
-                            {
-                              interview.buttons.map((button, index) => {
-                                return (
-                                  <a className="interview-button" style={{marginRight: index === interview.buttons.length-1 ? 0 : 30}} href={button.url} target="_blanck" key={button.title}>
-                                      {button.title}
-                                  </a>
-                                )
-                              })
-                            }
-                            </div>
+    <>
+      {
+        interviews.length === 0
+        ?
+          <section className="interviews-loading-container" id={props.id}>
+            <h2>Sin resultados</h2>
+          </section>
+        :
+          <section className="interviews-section" id={props.id}>
+            <div className="interviews-container">
+              {
+                interviews.slice(0,max_cards).map((interview) => {
+                  return (
+                    <div key={interview.title} className="interview-card">
+                      <h3 className="interview-title">
+                        {interview.title}
+                      </h3>
+                      <p className="interview-description">
+                        {interview.description}
+                      </p>
+                      <div className="interview-buttons">
+                      {
+                        interview.buttons.map((button, index) => {
+                          return (
+                            <a className="interview-button" style={{marginRight: index === interview.buttons.length-1 ? 0 : 30}} href={button.url} target="_blanck" key={button.title}>
+                                {button.title}
+                            </a>
+                          )
+                        })
+                      }
+                      </div>
 
-                            <img className="interview-image" alt="Proyecto" src={interview.image}/>
+                      <img className="interview-image" alt="Proyecto" src={interview.image}/>
 
-                          </div>
-                        )
-                      })
-                    }
-                  </div>              
-                  {
-                    max_cards !== interviews.length && interviews.length > max_cards ?
-                    <div key="plus" className="interview-card-plus" onClick={() => {setMaxCards(interviews.length)}}>
-                      <span className="interview-plus">+</span>
-                      <span className="interview-plus-text">Ver más entrevistas</span>
                     </div>
-                    : <></>
-                  }
-                  
-                </section>
+                  )
+                })
+              }
+            </div>              
+            {
+              max_cards !== interviews.length && interviews.length > max_cards ?
+              <div key="plus" className="interview-card-plus" onClick={() => {setMaxCards(interviews.length)}}>
+                <span className="interview-plus">+</span>
+                <span className="interview-plus-text">Ver más entrevistas</span>
+              </div>
+              : <></>
             }
-            </>
-        }
-    </div>
+            
+          </section>
+      }
+    </>
+
   )
 }
