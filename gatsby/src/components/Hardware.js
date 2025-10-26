6f9ed9b748fcebfa642c8a48e5e56793025efa7f
kikeelectronico <enrique.gomez.electronico@gmail.com>
2025-10-26 17:39:45 +0100

    remove loading flag

--- gatsby/src/components/Hardware.js
+++ gatsby/src/components/Hardware.js
@@ -57,70 +57,60 @@ export default function Hardware(props) {
   })
 
   return (
-    <>
-    {
-      projects.length === 0 
-      ?
-        <section className="project-loading-container" id={props.id}>
-          {/* <h2>Sin resultados</h2> */}
-        </section>
-      :
-        <section className="projects-section" id={props.id}>
-          <div className="projects-container">
-            {
-              projects.slice(0,max_cards).map((project) => {
-                
-                const baseName = project.parent.name 
-                const image = imagesMap[baseName]
-                
-                return (
-                  <div key={project.title} className="project-card">
-                    <div className="project-labels">
-                      {
-                        project.type.map(label => {
-                          return (<Label key={label} label={label}/>)
-                        })
-                      }
-                    </div>
-                    <h3 className="project-title">
-                      {project.title}
-                    </h3>
-                    <p className="project-description">
-                      {project.description}
-                    </p>
-                    <div className="project-buttons">
-                    {
-                      project.buttons.map((button, index) => {
-                        return (
-                          <a className="project-button" style={{marginRight: index === project.buttons.length-1 ? 0 : 30}} href={button.url} target="_blanck" key={button.title}>
-                              {button.title}
-                          </a>
-                        )
-                      })
-                    }
-                    </div>
+    <section className="projects-section" id={props.id}>
+      <div className="projects-container">
+        {
+          projects.slice(0,max_cards).map((project) => {
+            
+            const baseName = project.parent.name 
+            const image = imagesMap[baseName]
+            
+            return (
+              <div key={project.title} className="project-card">
+                <div className="project-labels">
+                  {
+                    project.type.map(label => {
+                      return (<Label key={label} label={label}/>)
+                    })
+                  }
+                </div>
+                <h3 className="project-title">
+                  {project.title}
+                </h3>
+                <p className="project-description">
+                  {project.description}
+                </p>
+                <div className="project-buttons">
+                {
+                  project.buttons.map((button, index) => {
+                    return (
+                      <a className="project-button" style={{marginRight: index === project.buttons.length-1 ? 0 : 30}} href={button.url} target="_blanck" key={button.title}>
+                          {button.title}
+                      </a>
+                    )
+                  })
+                }
+                </div>
 
-                    <GatsbyImage
-                        image={image}
-                        alt={`Imagen representativa de ${project.title}`}
-                        className="project-image"
-                      />
+                <GatsbyImage
+                    image={image}
+                    alt={`Imagen representativa de ${project.title}`}
+                    className="project-image"
+                  />
 
-                  </div>
-                )
-              })
-            }
-          </div>
-          {
-            max_cards !== projects.length && projects.length > max_cards ?
-            <button key="plus" className="project-card-plus" onClick={() => {setMaxCards(projects.length)}}>
-              <span className="project-plus">+</span>
-              <span className="project-plus-text">Ver más proyectos</span>
-            </button>
-            : <></>
-          }
-        </section>
-    }
-    </>
+              </div>
+            )
+          })
+        }
+      </div>
+      {
+        max_cards !== projects.length && projects.length > max_cards ?
+        <button key="plus" className="project-card-plus" onClick={() => {setMaxCards(projects.length)}}>
+          <span className="project-plus">+</span>
+          <span className="project-plus-text">Ver más proyectos</span>
+        </button>
+        : <></>
+      }
+    </section>
   )
 }
